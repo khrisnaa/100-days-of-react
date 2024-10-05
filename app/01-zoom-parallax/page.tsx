6440c57f3b3cd46daef53c5bde5843d15b3b5783
khrisnaa <109814050+khrisnaa@users.noreply.github.com>
2024-10-05 20:09:05 +0800

    fix: update lenis scroll position

--- app/01-zoom-parallax/page.tsx
+++ app/01-zoom-parallax/page.tsx
@@ -1,12 +1,11 @@
 import { ZoomParallax } from "@/app/01-zoom-parallax/components/zoom-parallax";
 import { ZoomParallaxSticky } from "@/app/01-zoom-parallax/components/zoom-parrallax-sticky";
-import { ReactLenis, useLenis } from "@/libs/react-lenis";
 
 export default function Page() {
   return (
-    <ReactLenis root>
+    <>
       <ZoomParallax />
       <ZoomParallaxSticky />
-    </ReactLenis>
+    </>
   );
 }
--- app/02-smooth-parallax/page.tsx
+++ app/02-smooth-parallax/page.tsx
@@ -1,10 +1,5 @@
 import { ScrollGallery } from "./components/scroll-gallery";
-import { ReactLenis } from "@/libs/react-lenis";
 
 export default function Page() {
-  return (
-    <ReactLenis root>
-      <ScrollGallery />
-    </ReactLenis>
-  );
+  return <ScrollGallery />;
 }
